deede53050f74761588553a0e65b2f82872d5ae5
Js <13782017+JimmyLoveSiren@users.noreply.github.com>
2019-04-16 19:40:22 +0900

    config.json

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
 'use strict';
 require('log-timestamp');
+console.log('starting server');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
+const ect = require('ect');
 const express = require('express');
 const request = require('request');
 
-const config = {
-	port: 3000,
-	slack: 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX',
-	upsource: 'http://upsource.server',
-	color: '#fe8202'
-};
+const config = require('./config.json');
+_.defaults(config, {port: 3000});
+config.presets = _.chain(config.presets)
+	.mapKeys((preset, name) => _.toLower(name))
+	.pickBy((preset, name) => {
+		if (isValid(preset)) return true;
+		console.error('invalid preset: ' + name);
+		return false;
+	}).value();
+
+const page = ect({root: __dirname}).render('template.ect', {
+	presets: _.entries(config.presets)
+});
 
 const app = express();
 app.use(bodyParser.json());
-app.get('*', (req, res) => {
-	res.sendFile('index.html', {root: 'www'},
-		err => err && res.sendStatus(404));
+app.get('*', (req, res) => res.send(page));
+_.each(config.presets, (preset, name) => {
+	app.post('*/' + name, (req, res) => {
+		_.defaults(req.query, preset);
+		handle(req, res);
+	});
 });
-app.post('*', (req, res) => {
-	const payload = upsourceToSlack(req.body);
-	if (payload) {
-		request.post({url: config.slack, json: payload});
-	}
-	res.end();
-});
+app.post('*', handle);
 app.listen(config.port);
 console.log('listening on port ' + config.port);
 
-function upsourceToSlack(body) {
+function isValid(query) {
+	const slack = _.get(query, 'slack');
+	if (!_.isString(slack)) return false;
+	if (!slack.startsWith('https://hooks.slack.com/services/')) return false;
+	return true;
+}
+
+function handle(req, res) {
+	if (!isValid(req.query)) {
+		console.error('bad query: ' + req.query);
+		res.sendStatus(400);
+		return;
+	}
+	const payload = generatePayload(req.body, req.query);
+	if (_.isEmpty(payload)) {
+		res.sendStatus(501);
+		return;
+	}
+	payload.channel = req.query.channel;
+	request.post({url: req.query.slack, json: payload}, (err, slackRes, slackBody) => {
+		if (err) {
+			console.error(err);
+			res.sendStatus(500);
+			return;
+		}
+		console.log(slackRes.statusCode);
+		res.sendStatus(slackRes.statusCode);
+	});
+}
+
+function generatePayload(body, query) {
 	const dataType = _.get(body, 'dataType');
 	if (dataType === 'DiscussionFeedEventBean') {
 		const projectId = _.get(body, 'projectId');
@@ -36,13 +72,13 @@ function upsourceToSlack(body) {
 		const userName = _.get(body, 'data.base.actor.userName');
 		const commentId = _.get(body, 'data.commentId');
 		const commentText = _.get(body, 'data.commentText');
-		const baseUrl = `${config.upsource}/${projectId}/review/${reviewId}`;
+		const baseUrl = `${query.upsource}/${projectId}/review/${reviewId}`;
 		console.log('new comment');
 		return {
 			attachments: [{
 				fallback: `[${projectId}/${reviewId}] New comment by ${userName}`,
 				pretext: `[<${baseUrl}|${projectId}/${reviewId}>] <${baseUrl}?commentId=${commentId}|New comment> by ${userName}`,
-				color: config.color,
+				color: query.color,
 				text: commentText,
 				mrkdwn_in: ['text']
 			}]
